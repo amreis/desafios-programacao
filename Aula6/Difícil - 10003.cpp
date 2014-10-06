1b47dba455a627a2022c2f8eca7976712f0a86ae
Álister Machado dos Reis <alister.reis@gmail.com>
2014-10-05 21:06:40 -0300

    Update Difícil - 10003.cpp
    Added documentation.

--- Aula6/Difícil - 10003.cpp
+++ Aula6/Difícil - 10003.cpp
@@ -19,19 +19,24 @@ int main()
             cin >> cuts[_n];
         }
         cuts[n+1] = l;
-
+        // The idea here is to run through the diagonals, so we are always calculating
+        // [i][i+k], for the kth diagonal.
         for (int step = 1; step <= n + 1; ++step)
         {
+            // We must go through every row of the matrix where we can access the row + step column
             for (int row = 0; row + step <= n + 1; ++row)
             {
+                // For the first diagonal, we can't make cuts, so cost zero.
                 if (step == 1) pd[row][row+step] = 0;
                 else
                 {
                     int m = INT_MAX;
+                    // Minimize cost of next cut location.
                     for (int k = row+1; k < row + step; ++k)
                     {
                         m = min(m, pd[row][k] + pd[k][row+step]);
                     }
+                    // Save it.
                     pd[row][row+step] = m + cuts[row+step]-cuts[row];
                 }
             }
